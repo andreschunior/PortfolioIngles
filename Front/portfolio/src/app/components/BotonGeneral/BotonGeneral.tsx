f2f1d3ecd6238654e8af961cbbc17cc991de4b3b
andreschunior <andres23540575@gmail.com>
2024-08-25 20:47:51 -0400

    actualizacion se cambio el texto del boton para ver si se corrige en la version de movil

--- Front/portfolio/src/app/components/BotonGeneral/BotonGeneral.tsx
+++ Front/portfolio/src/app/components/BotonGeneral/BotonGeneral.tsx
@@ -9,7 +9,7 @@ const BotonConIcono: React.FC<BotonConIconoProps> = ({
   return (
     <button
       onClick={onClick}
-      className="py-2 px-6 rounded-full text-xl font-semibold border-2 transition-transform transform hover:scale-105 flex items-center"
+      className="py-2 px-6 rounded-full text-xl font-semibold border-2 transition-transform transform hover:scale-105 flex items-center text-black !important "
       style={{
         borderImage: "linear-gradient(45deg, #6ee7b7, #3b82f6) 1",
         borderColor: "transparent",
